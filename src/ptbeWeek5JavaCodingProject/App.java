1cd03c5c50bf6305b967cc67aa422adb94acf5b8
Becky Van Vickle <opticalchick@yahoo.com>
2024-05-01 12:02:07 -0500

    added comments

--- src/ptbeWeek5JavaCodingProject/App.java
+++ src/ptbeWeek5JavaCodingProject/App.java
@@ -4,19 +4,23 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		String str = "Hello";
+		String str = "Goodbye!";
 		Logger asteriskLogger = new AsteriskLogger();
 		Logger spacedLogger = new SpacedLogger();
 		
 		asteriskLogger.log(str);
 		System.out.println("\n");
+		
 		asteriskLogger.error(str);
 		System.out.println("\n");
+		
 		System.out.println("-----------------------------------");
+		
 		System.out.println("\n");
-		spacedLogger.log("Spaced out.");
+		spacedLogger.log(str);
+		
 		System.out.println("\n");
-		spacedLogger.error("Spaced out error.");
+		spacedLogger.error(str);
 
 	}
 
--- src/ptbeWeek5JavaCodingProject/AsteriskLogger.java
+++ src/ptbeWeek5JavaCodingProject/AsteriskLogger.java
@@ -12,14 +12,14 @@ public class AsteriskLogger implements Logger{
 	public void error(String str) {
 		String error = "Error: " + str;
 		int length = error.length() + 6;
-		StringBuilder line = new StringBuilder();
+		StringBuilder errorLine = new StringBuilder();
 		
 		for (int i = 1; i <= length; i++) {
-			line.append("*");
+			errorLine.append("*");
 		}
 		
-		System.out.println(line.toString());
-		log(error); System.out.println(line.toString());
+		System.out.println(errorLine.toString());
+		log(error); System.out.println(errorLine.toString());
 		
 	}
 
